5523e3d8e997f77a1ce92d357c83760674436007
jubayer <businesslab5556@gmail.com>
2025-12-31 17:14:51 +0600

    Update tsconfig and index.ts with server code

---
+++ backend/src/index.ts
@@ -0,0 +1,22 @@
+import express, { Request, Response } from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('MyMessagingApp Backend is Running! 🚀');
+});
+
+app.listen(PORT, () => {
+  console.log('-----------------------------------------');
+  console.log('✅ Server is running on: http://localhost:' + PORT);
+  console.log('✅ .env Port Check: ' + process.env.PORT);
+  console.log('-----------------------------------------');
+});
